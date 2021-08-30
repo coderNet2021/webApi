86b9f1a6a14cacb314d11385fdabb26fb7008677
coderNet2021 <cnss.saida@gmail.com>
2021-08-30 15:19:49 +0300

    untill video 50

--- myWebApiProject/Controllers/ValuesController.cs
+++ myWebApiProject/Controllers/ValuesController.cs
@@ -7,26 +7,38 @@ using System.Threading.Tasks;
 
 namespace myWebApiProject.Controllers
 {
-   // [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [Route("api/get-all")]
+        //[Route("api/get-all")]
+        //[Route("getall")]
+        //[Route("get-all")]
+        //[Route("[controller]/[action]")]
+        //overrite : //[Route("~/get-all")]
         public string GetAll() {
             return "Hello from get All";
         }
 
-        [Route("api/get-all-authors")]
+        //[Route("api/get-all-authors")]
+        //[Route("getall")] this is not possible
         public string GetAllAuthors()
         {
             return "Hello from get All authors";
         }
 
-        [Route("books/{id}")]
-        public string GetById(int id)
+        //[Route("books/{id}/author/{authorId}")]
+        [Route("{id}")]
+        public string GetById(int id ,int authorId)
         {
-            return "Hello : "+id;
+            return "Hello : " + id + " author Id = " + authorId;
         }
 
+
+        //[Route("search")]
+        public string SearchBooks(int id, int authorId, string name, int rating, int price)
+        {
+            return "Hello : " + id + " author Id = " + authorId+ " name  = "+name;
+        }
     }
 }
